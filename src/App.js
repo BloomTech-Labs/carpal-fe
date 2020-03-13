a165eb9e5eb78a88ab5b3524f28c3a06693f63f8
smv5047 <smv5047@gmail.com>
2020-03-12 20:38:29 -0400

    useEffect setup to pull user data - form started

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import ProfilePage from "./Components/Profile-Pages/Profile-Pages";
 
 function App() {
     return (
         <div className="App">
             <h1>CarPal</h1>
+            <ProfilePage />
         </div>
     );
 }
--- src/Components/Profile-Pages/Profile-Pages.js
+++ src/Components/Profile-Pages/Profile-Pages.js
@@ -1,3 +1,37 @@
-import React, {useState, useEffect} from "React"
-import {Form, withFormik, Field} from "Formik"
-import * from "yup"
+import React, { useState, useEffect } from "React";
+import { Form, withFormik, Field } from "Formik";
+import * as yup from "yup";
+import axios from "axios";
+
+function ProfilePage() {
+    const [user, setUser] = useState({});
+    const [load, setLoad] = useState(false);
+    const [error, setError] = useState("");
+
+    useEffect(() => {
+        if (!user) {
+            axios
+                .get(`https://carpal-${process.NODE_ENV}.herokuapp.com/}`)
+                .then(res => {
+                    setUser([...users, status]);
+                    setLoad(true);
+                })
+                .catch(err => {
+                    setError(err.message);
+                    setLoad(true);
+                });
+        }
+    }, [user]);
+
+    return (
+        <>
+            <img src="${user.profile_picture}"></img>
+            <div></div>
+            <Form>
+                <Field name="first_name" type="text" placeholder="First name" />
+            </Form>
+        </>
+    );
+}
+
+module.exports = ProfilePage;
