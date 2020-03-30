cce4f9b2dc0792ba5b05ffe4f6ec71733242eb75
Dangus <dangus1924@gmail.com>
2020-03-30 18:16:32 -0400

    Wroked on landing page

--- src/Components/Landing-Page/Landing-Page.js
+++ src/Components/Landing-Page/Landing-Page.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Slider from "../../img/CarPal.mp4";
 import Imgone from "../../img/Group 38 (1).png";
 import Imgtwo from "../../img/Group 29.png";
-import Imgthree from "../../img/Group 39.jpg";
+import Imgthree from "../../img/Group 39.png";
 import dashboard from "../../App"
 
 import "./Landing-Page.scss";
@@ -32,7 +32,7 @@ function LandingPage() {
                     <h2>Features</h2>
                     <div className="features">            
                         <div className="paraone">            
-                            <p className="firstpara">
+                            <p className="para">
                             With this awesome app, these are just a few of the features you will get such as
                             SMS messaging, friends list, gamification (user stats and point system) and even
                             driver's real-time location, but wait there's more and its just five clicks 
@@ -50,7 +50,7 @@ function LandingPage() {
                     <h2>Drivers</h2>
                     <div className="featuresflip">
                         <div className="paraone">            
-                            <p className="firstpara">
+                            <p className="para">
                             Are you a current driver who loves to drive or even hates to drive,
                             well guess what you in luck. The Carpal developer kept you in mind 
                             when building this app I might even add that they made it just for you.    
@@ -66,7 +66,7 @@ function LandingPage() {
                     <h2>Riders</h2>
                     <div className="features">
                         <div className="paraone">            
-                            <p className="firstpara">
+                            <p className="para">
                             As a rider you gain many great features and here are just a few of those. Request 
                             a ride to your destination, customize the type of driver you are interested in 
                             meet, and rate each Carpal experience to make sure our community stays the best 
@@ -74,7 +74,7 @@ function LandingPage() {
                             </p>
                         </div>
                         <div className="imgone">
-                            <img className="allimgbot" src={Imgthree} alt="Temp img" /> 
+                            <img className="allimg" src={Imgthree} alt="Temp img" /> 
                         </div>     
                     </div>
                     
