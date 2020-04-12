5983878930fa043623e9be9ee21460467bd5a088
lesley <lesleybanadzem2017@gmail.com>
2020-04-12 12:14:03 -0700

    Added a utility function to extract extra code for initializing axios

--- src/Utils/Api.js
+++ src/Utils/Api.js
@@ -1,17 +1,21 @@
 import axios from "axios";
-import dotenv from 'dotenv'
-dotenv.config()
-export default function() {
-    console.log(process.env.NODE_ENV)
+import dotenv from "dotenv";
+dotenv.config();
+export default function () {
+    console.log(process.env.NODE_ENV);
 
     //react starts in development mode automatically
-    if (process.env.NODE_ENV === "development") {
+    function axiosInstance(env) {
         return axios.create({
             baseURL: "https://staging-carpal.herokuapp.com/",
             headers: {
+                enviroment: env,
                 authorization: localStorage.getItem("token")
             }
         });
+    }
+    if (process.env.NODE_ENV === "development") {
+        return axiosInstance("development");
     } else if (process.env.NODE_ENV === "production") {
         return axios.create({
             baseURL: "https://carpal-production.herokuapp.com/",
@@ -19,5 +23,7 @@ export default function() {
                 authorization: localStorage.getItem("token")
             }
         });
+    } else if (process.env.NODE_ENV === "staging") {
+        return axiosInstance("staging");
     }
 }
