cf5f1db06870a83f60caa791360fe742ab050e8d
smv5047 <smv5047@gmail.com>
2020-04-14 18:29:29 -0400

    refactored profile , created profile header component, started tests for both

--- src/Components/Profile-Pages/Profile-Pages.js
+++ src/Components/Profile-Pages/Profile-Pages.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import UpdateProfile from "./UpdateProfile";
 import "./Profile-Pages.scss";
-import userDetail from "./userDetail";
+import ProfileHeader from "./ProfileHeader";
+// import userDetail from "./userDetail";
 
 import { connect } from "react-redux";
 
@@ -11,12 +12,7 @@ import {
 } from "../../Redux/Actions/UserAction";
 import MapBox from "../MapBox/MapBox";
 
-//TODO - Test Use Effect with Seed Data
-//TODO - Setup input for image, and coordinate with BE for storage via S3 bucket
-//TODO - Create Loading Spinner Component
-
 function ProfilePage(props) {
-    // const { errors, touched } = props;
     const [user, setUser] = useState({});
 
     useEffect(() => {
@@ -44,30 +40,7 @@ function ProfilePage(props) {
                     {user.phone_number ? (
                         // on profile page, if user already has a phone number (stand in for profile),
                         <>
-                            <div className="profileHeader">
-                                <div className="headerImage">
-                                    <img
-                                        className="profilePic"
-                                        src="https://pbs.twimg.com/profile_images/1232869769013014535/iwN5kET4_400x400.jpg"
-                                        alt="img1"
-                                    ></img>
-                                    <a
-                                        className="overlaybutton"
-                                        href="#"
-                                        alt="Profile"
-                                    >
-                                        Upload Image
-                                    </a>
-                                </div>
-                                <div className="headerDetails">
-                                    <h3 role="header name" className="bold">
-                                        {user.first_name}&nbsp;
-                                        {user.last_name}
-                                    </h3>
-                                    <h3>{user.email}</h3>
-                                    <h3>{user.phone_number}</h3>
-                                </div>
-                            </div>
+                            <ProfileHeader />
                             <div className="bar"></div>
                             <div className="profileDetails">
                                 {user.is_driver ? (
@@ -82,10 +55,7 @@ function ProfilePage(props) {
 
                                     <userDetail title="Audio I hate" item={user.audio_hate} /> */}
                                 </div>
-                                {/* profileSection */}
-
                                 <MapBox />
-
                                 <div className="buttonContainer">
                                     <button
                                         className="edit"
@@ -95,7 +65,6 @@ function ProfilePage(props) {
                                     </button>
                                 </div>
                             </div>
-                            {/* profileDetails */}
                         </>
                     ) : (
                         // on profile page, if user doesn't have phone number (stand in for profile), redirect to form?
