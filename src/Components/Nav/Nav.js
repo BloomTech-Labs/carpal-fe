59e7755d2a105223e37b8ff36454f974f1aa13fe
Carlos <miklo.concepcion@gmail.com>
2020-04-17 22:37:17 -0500

    Functionality of user being logged in renders users first name. if user is not logged in p tag renders not logged in in footer nav. icons are set with HomeIcon on successful authentication. Unsuccessful has homeIconInverse rendering.

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,29 +1,49 @@
-import React, { useState } from "react";
+import React from "react";
 import { NavLink } from "react-router-dom";
 import HomeIcon from "../../img/footer-icons/HomeIcon.png";
-import HomeIconinverse from "../../img/footer-icons/HomeIcon-Inverse.png";
+import homeIconInverse from "../../img/footer-icons/HomeIcon-Inverse.png";
 
 import "./Nav.scss";
 
-export default function Nav() {
+export default function Nav(props) {
+    const RiderOrDriver = (auth) => {
+        return auth.isAuthenticated ? (
+            <nav className="bottom-nav">
+                <NavLink to="/dashboard">
+                    <img
+                        className="profilePic"
+                        src={HomeIcon}
+                        alt="Patchy"
+                        style={{
+                            width: "40px",
+                            height: "40px"
+                        }}
+                    />
+                </NavLink>
+                <p>footer logged in{auth.first_name}</p>
+            </nav>
+        ) : (
+            <nav className="bottom-nav">
+                <NavLink to="/dashboard">
+                    <img
+                        className="profilePic"
+                        src={HomeIcon}
+                        alt="Patchy"
+                        style={{
+                            width: "40px",
+                            height: "40px"
+                        }}
+                    />
+                </NavLink>
+            </nav>
+        );
+    };
     return (
         <nav className="bottom-nav">
-            <NavLink to="/">
-                {" "}
-                <img
-                    className="profilePic"
-                    src={HomeIconinverse}
-                    alt="Patchy"
-                    style={{
-                        width: "40px",
-                        height: "40px"
-                    }}
-                />
-            </NavLink>
             <NavLink to="/dashboard">
                 <img
                     className="profilePic"
-                    src={HomeIcon}
+                    src={homeIconInverse}
                     alt="Patchy"
                     style={{
                         width: "40px",
@@ -31,18 +51,18 @@ export default function Nav() {
                     }}
                 />
             </NavLink>
-            <NavLink to="/">
-                {" "}
-                <img
-                    className="profilePic"
-                    src={HomeIconinverse}
-                    alt="Patchy"
-                    style={{
-                        width: "40px",
-                        height: "40px"
-                    }}
-                />
-            </NavLink>
+            <p
+                style={{
+                    color: "#ffffff",
+                    fontSize: "large"
+                }}
+            >
+                not logged in
+            </p>
         </nav>
     );
 }
+
+// Nav.PropTypes = {
+//     auth: React.PropTypes.object.isRequired
+// };
