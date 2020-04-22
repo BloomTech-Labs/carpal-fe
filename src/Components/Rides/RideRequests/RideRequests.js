5125649fb3f0022a1ef5757c461dbee6534650d4
smv5047 <smv5047@gmail.com>
2020-04-21 21:51:33 -0400

    onclick toggles between requests displaying and not displaying

--- src/Components/Rides/RideRequests/RideRequests.js
+++ src/Components/Rides/RideRequests/RideRequests.js
@@ -1,5 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function RideRequests() {
-    return <></>;
+import "./RideRequests.scss";
+
+export default function RideRequests(props) {
+    const [isIncomingRequestsOpen, setIsIncomingRequestsOpen] = useState(false);
+    const [isOutgoingRequestsOpen, setIsOutgoingRequestsOpen] = useState(false);
+
+    return (
+        <>
+            <div>
+                <h3>Requests</h3>
+            </div>
+            <div>
+                <h3>Incoming</h3>
+                <button
+                    onClick={() =>
+                        setIsIncomingRequestsOpen(!isIncomingRequestsOpen)
+                    }
+                >
+                    ...
+                </button>
+                {isIncomingRequestsOpen && <div>Incoming Request 1</div>}
+            </div>
+
+            <div>
+                <div>Outgoing</div>
+                <button
+                    onClick={() =>
+                        setIsOutgoingRequestsOpen(!isOutgoingRequestsOpen)
+                    }
+                    className="dotExpander"
+                >
+                    ...
+                </button>
+                {isOutgoingRequestsOpen && <div>Outgoing Request 1</div>}
+            </div>
+        </>
+    );
 }
