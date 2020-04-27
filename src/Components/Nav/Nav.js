0b700dbffef7827ce8010e244cb502af38dd64d2
Carl Redding <credding07@gmail.com>
2020-04-27 15:32:14 -0500

    tore it up and re-wiring it with withRouter for react-redux useage. cleaning up the code more. links work, just need to work on user authentication

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,24 +1,51 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, withRouter } from "react-router-dom";
 //icons for nav
 import HomeIcon from "../../img/footer-icons/HomeIcon.png";
-import CarPalReq from "../../img/footer-icons/CarPalReq-Inverted.png";
+// import homeIconInverse from "../../img/footer-icons/HomeIcon-Inverse.png";
+// import PalReq from "../../img/footer-icons/CarPalReq-Inverted.png";
 import FindRide from "../../img/footer-icons/RideRequest-Inverse.png";
+import RequestIcon from "../../img/footer-icons/CarPalReq2-Inverted.png";
 
 import "./Nav.scss";
 
-export default function Nav() {
-    const userAuthorized = localStorage.getItem("token");
+function Nav() {
+    const isAuthenticated = localStorage.getItem("token");
     return (
         <>
             <nav className="bottom-nav">
                 {/* home path */}
-                {!userAuthorized ? (
+                {!isAuthenticated ? (
                     <>
                         <NavLink to="/dashboard">
                             <img
                                 className="profilePic"
                                 src={HomeIcon}
+                                alt="Patchy"
+                                style={{
+                                    width: "40px",
+                                    height: "40px"
+                                }}
+                            />
+                        </NavLink>
+
+                        {/* path to request a ride */}
+                        <NavLink to="/riderequest">
+                            <img
+                                className="profilePic"
+                                src={RequestIcon}
+                                alt="Request"
+                                style={{
+                                    width: "40px",
+                                    height: "40px"
+                                }}
+                            />
+                        </NavLink>
+                        {/* path to search for a ride */}
+                        <NavLink to="/home">
+                            <img
+                                className="profilePic"
+                                src={FindRide}
                                 alt="Patchy"
                                 style={{
                                     width: "40px",
@@ -29,7 +56,7 @@ export default function Nav() {
                     </>
                 ) : (
                     <>
-                        <NavLink to="/dashboard">
+                        <NavLink to="/">
                             <img
                                 className="profilePic"
                                 src={HomeIcon}
@@ -40,23 +67,11 @@ export default function Nav() {
                                 }}
                             />
                         </NavLink>
-                        {/* // path to request a ride */}
+
                         <NavLink to="/home">
                             <img
                                 className="profilePic"
                                 src={FindRide}
-                                alt="Request"
-                                style={{
-                                    width: "40px",
-                                    height: "40px"
-                                }}
-                            />
-                        </NavLink>
-                        {/* // path to search for a ride */}
-                        <NavLink to="/riderequest">
-                            <img
-                                className="profilePic"
-                                src={CarPalReq}
                                 alt="Patchy"
                                 style={{
                                     width: "40px",
@@ -70,3 +85,5 @@ export default function Nav() {
         </>
     );
 }
+
+export default withRouter(Nav);
