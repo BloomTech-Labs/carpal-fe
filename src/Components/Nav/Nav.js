c24221aefa18c82af63362e7484571a6474f857d
Carl Redding <credding07@gmail.com>
2020-04-27 14:02:46 -0500

    added more jsx <> </> tags around elements for functionality

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -2,32 +2,31 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 //icons for nav
 import HomeIcon from "../../img/footer-icons/HomeIcon.png";
-// import homeIconInverse from "../../img/footer-icons/HomeIcon-Inverse.png";
 import CarPalReq from "../../img/footer-icons/CarPalReq-Inverted.png";
 import FindRide from "../../img/footer-icons/RideRequest-Inverse.png";
-// import RequestIcon from "../../img/footer-icons/CarPalReq2-Inverted.png";
 
 import "./Nav.scss";
 
 export default function Nav(props) {
-    const userAuth = localStorage.getItem("token");
+    const userAuthorized = localStorage.getItem("token");
     return (
         <>
             <nav className="bottom-nav">
                 {/* home path */}
-                {!userAuth ? (
-                    // <>
-                    <NavLink to="/dashboard">
-                        <img
-                            className="profilePic"
-                            src={HomeIcon}
-                            alt="Patchy"
-                            style={{
-                                width: "40px",
-                                height: "40px"
-                            }}
-                        />
-                    </NavLink>
+                {!userAuthorized ? (
+                    <>
+                        <NavLink to="/dashboard">
+                            <img
+                                className="profilePic"
+                                src={HomeIcon}
+                                alt="Patchy"
+                                style={{
+                                    width: "40px",
+                                    height: "40px"
+                                }}
+                            />
+                        </NavLink>
+                    </>
                 ) : (
                     <>
                         <NavLink to="/dashboard">
@@ -42,7 +41,7 @@ export default function Nav(props) {
                             />
                         </NavLink>
                         {/* // path to request a ride */}
-                        <NavLink to="/riderequest">
+                        <NavLink to="/home">
                             <img
                                 className="profilePic"
                                 src={FindRide}
@@ -54,7 +53,7 @@ export default function Nav(props) {
                             />
                         </NavLink>
                         {/* // path to search for a ride */}
-                        <NavLink to="/home">
+                        <NavLink to="/riderequest">
                             <img
                                 className="profilePic"
                                 src={CarPalReq}
@@ -67,17 +66,6 @@ export default function Nav(props) {
                         </NavLink>
                     </>
                 )}
-                {/* <NavLink to="/dashboard">
-                    <img
-                        className="profilePic"
-                        src={HomeIcon}
-                        alt="Patchy"
-                        style={{
-                            width: "40px",
-                            height: "40px"
-                        }}
-                    />
-                </NavLink> */}
             </nav>
         </>
     );
