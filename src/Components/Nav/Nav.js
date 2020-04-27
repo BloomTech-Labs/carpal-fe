dde74c55f90eac244d5755642a207e5b386833b5
Carl Redding <credding07@gmail.com>
2020-04-27 13:57:33 -0500

    cleaned up logic and functionality of component. DRY 2020, still need to implement full testing for components functionality.

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -2,108 +2,83 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 //icons for nav
 import HomeIcon from "../../img/footer-icons/HomeIcon.png";
-import homeIconInverse from "../../img/footer-icons/HomeIcon-Inverse.png";
-import PalReq from "../../img/footer-icons/CarPalReq-Inverted.png";
+// import homeIconInverse from "../../img/footer-icons/HomeIcon-Inverse.png";
+import CarPalReq from "../../img/footer-icons/CarPalReq-Inverted.png";
 import FindRide from "../../img/footer-icons/RideRequest-Inverse.png";
-import RequestIcon from "../../img/footer-icons/CarPalReq2-Inverted.png";
+// import RequestIcon from "../../img/footer-icons/CarPalReq2-Inverted.png";
 
 import "./Nav.scss";
 
 export default function Nav(props) {
-    const UserType = (auth) => {
-        return auth.isAuthenticated ? (
-            <nav className="bottom-nav">
-                <p>footer logged in{auth.first_name}</p>
-                {/* home path */}
-                <NavLink to="/dashboard">
-                    <img
-                        className="profilePic"
-                        src={HomeIcon}
-                        alt="Patchy"
-                        style={{
-                            width: "40px",
-                            height: "40px"
-                        }}
-                    />
-                </NavLink>
-                {/* path to search for a ride */}
-                <NavLink to="/ridefind">
-                    <img
-                        className="profilePic"
-                        src={FindRide}
-                        alt="Patchy"
-                        style={{
-                            width: "40px",
-                            height: "40px"
-                        }}
-                    />
-                </NavLink>
-                {/* path to request a ride */}
-                <NavLink to="/riderequest">
-                    <img
-                        className="profilePic"
-                        src={RequestIcon}
-                        alt="Request"
-                        style={{
-                            width: "40px",
-                            height: "40px"
-                        }}
-                    />
-                </NavLink>
-            </nav>
-        ) : (
-            <nav className="bottom-nav">
-                <NavLink to="/dashboard">
-                    <img
-                        className="profilePic"
-                        src={HomeIcon}
-                        alt="Patchy"
-                        style={{
-                            width: "40px",
-                            height: "40px"
-                        }}
-                    />
-                </NavLink>
-            </nav>
-        );
-    };
+    const userAuth = localStorage.getItem("token");
     return (
-        <nav className="bottom-nav">
-            <p
-                style={{
-                    color: "#ffffff",
-                    fontSize: "large"
-                }}
-            >
-                not logged in
-            </p>
-            <NavLink to="/">
-                <img
-                    className="profilePic"
-                    src={HomeIcon}
-                    alt="Patchy"
-                    style={{
-                        width: "40px",
-                        height: "40px"
-                    }}
-                />
-            </NavLink>
-
-            <NavLink to="/dashboard">
-                <img
-                    className="profilePic"
-                    src={FindRide}
-                    alt="Patchy"
-                    style={{
-                        width: "40px",
-                        height: "40px"
-                    }}
-                />
-            </NavLink>
-        </nav>
+        <>
+            <nav className="bottom-nav">
+                {/* home path */}
+                {!userAuth ? (
+                    // <>
+                    <NavLink to="/dashboard">
+                        <img
+                            className="profilePic"
+                            src={HomeIcon}
+                            alt="Patchy"
+                            style={{
+                                width: "40px",
+                                height: "40px"
+                            }}
+                        />
+                    </NavLink>
+                ) : (
+                    <>
+                        <NavLink to="/dashboard">
+                            <img
+                                className="profilePic"
+                                src={HomeIcon}
+                                alt="Patchy"
+                                style={{
+                                    width: "40px",
+                                    height: "40px"
+                                }}
+                            />
+                        </NavLink>
+                        {/* // path to request a ride */}
+                        <NavLink to="/riderequest">
+                            <img
+                                className="profilePic"
+                                src={FindRide}
+                                alt="Request"
+                                style={{
+                                    width: "40px",
+                                    height: "40px"
+                                }}
+                            />
+                        </NavLink>
+                        {/* // path to search for a ride */}
+                        <NavLink to="/home">
+                            <img
+                                className="profilePic"
+                                src={CarPalReq}
+                                alt="Patchy"
+                                style={{
+                                    width: "40px",
+                                    height: "40px"
+                                }}
+                            />
+                        </NavLink>
+                    </>
+                )}
+                {/* <NavLink to="/dashboard">
+                    <img
+                        className="profilePic"
+                        src={HomeIcon}
+                        alt="Patchy"
+                        style={{
+                            width: "40px",
+                            height: "40px"
+                        }}
+                    />
+                </NavLink> */}
+            </nav>
+        </>
     );
 }
-//Typo in static class property declaration  (error message when uncommented.)
-// Nav.PropTypes : {
-//     auth: React.PropTypes.object.isRequired
-// };
