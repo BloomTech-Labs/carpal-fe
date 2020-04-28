f453fcc597a7346ca5d2daa88670d9ca83e817a3
Carl Redding <credding07@gmail.com>
2020-04-28 16:28:34 -0500

    functionality of component is working on my local machine. Not logged in you will see three icons on the footer. if you are logged in you will see two just like the figma.

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,22 +1,31 @@
 import React from "react";
 import { NavLink, withRouter } from "react-router-dom";
-//icons for nav
+//icons for footer-nav
 import HomeIcon from "../../img/footer-icons/HomeIcon.png";
-// import homeIconInverse from "../../img/footer-icons/HomeIcon-Inverse.png";
-// import PalReq from "../../img/footer-icons/CarPalReq-Inverted.png";
 import FindRide from "../../img/footer-icons/RideRequest-Inverse.png";
-import RequestIcon from "../../img/footer-icons/CarPalReq2-Inverted.png";
+import CarPalReq from "../../img/footer-icons/CarPalReq-Inverted.png";
 
 import "./Nav.scss";
 
 function Nav() {
     const isAuthenticated = localStorage.getItem("token");
+
     return (
         <>
             <nav className="bottom-nav">
-                {/* home path */}
                 {!isAuthenticated ? (
                     <>
+                        <NavLink to="/riderequest">
+                            <img
+                                className="profilePic"
+                                src={CarPalReq}
+                                alt="Request"
+                                style={{
+                                    width: "40px",
+                                    height: "40px"
+                                }}
+                            />
+                        </NavLink>
                         <NavLink to="/dashboard">
                             <img
                                 className="profilePic"
@@ -28,19 +37,6 @@ function Nav() {
                                 }}
                             />
                         </NavLink>
-                        {/* path to request a ride
-                        <NavLink to="/riderequest">
-                            <img
-                                className="profilePic"
-                                src={RequestIcon}
-                                alt="Request"
-                                style={{
-                                    width: "40px",
-                                    height: "40px"
-                                }}
-                            />
-                        </NavLink> */}
-                        {/* path to search for a ride */}
                         <NavLink to="/home">
                             <img
                                 className="profilePic"
@@ -66,7 +62,6 @@ function Nav() {
                                 }}
                             />
                         </NavLink>
-
                         <NavLink to="/home">
                             <img
                                 className="profilePic"
@@ -78,6 +73,18 @@ function Nav() {
                                 }}
                             />
                         </NavLink>
+
+                        {/* <NavLink to="/riderequest">
+                            <img
+                                className="profilePic"
+                                src={RequestIcon}
+                                alt="Request"
+                                style={{
+                                    width: "40px",
+                                    height: "40px"
+                                }}
+                            />
+                        </NavLink> */}
                     </>
                 )}
             </nav>
