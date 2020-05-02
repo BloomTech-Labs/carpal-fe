83639481224eee552291997e09f3d885e99f106c
lesley <lesleybanadzem2017@gmail.com>
2020-05-01 20:29:51 -0700

    wrote couple more assertions to to test deferent elements

--- src/Components/Rides/SavedRide/SavedRide.js
+++ src/Components/Rides/SavedRide/SavedRide.js
@@ -1,50 +1,51 @@
-import React, { useState, useEffect } from 'react'
-import SavedRideCard from '../SavedRideCard/SavedRideCard'
-import { connect } from 'react-redux'
-import AddLocationName from '../SavedRideCard/AddLocationName'
+import React, { useState, useEffect } from "react";
+import SavedRideCard from "../SavedRideCard/SavedRideCard";
+import { connect } from "react-redux";
+import AddLocationName from "../SavedRideCard/AddLocationName";
 import "./SavedRide.scss";
 
-
-
 function SavedRides(props) {
-    const [show, setShow] = useState(false)
-    const [rides, setRides] = useState()
+    const [show, setShow] = useState(false);
+    const [rides, setRides] = useState();
 
     useEffect(() => {
-        setRides(props.savedRides)
-    }, [props.savedRides])
+        setRides(props.savedRides);
+    }, [props.savedRides]);
 
     const toggleClass = () => {
-        setShow(!show)
-        console.log(show)
-    }
+        setShow(!show);
+      
+    };
 
-    console.log(props.savedRides)
-
-
+    
     return (
         <div>
-            {show ? (<AddLocationName toggle={toggleClass} />) : (< div className='saved-rides' >
-                <section className='my-saved'>
-                    <h1>My saved rides...</h1>
-                    <button onClick={toggleClass}>Add New ride</button>
-                </section>
-                {/* <SavedRideCard /> */}
+            {show ? (
+                <AddLocationName toggle={toggleClass} />
+            ) : (
+                <div className="saved-rides">
+                    <section className="my-saved">
+                        <h1>My saved rides...</h1>
+                        <button onClick={toggleClass}>Add New ride</button>
+                    </section>
+                    {/* <SavedRideCard /> */}
 
-                {props.savedRides.map((rideData, index) => <SavedRideCard key={index} data={rideData} rides={rides} setRides={setRides} />)}
-
-            </div >)}
+                    {props.savedRides.map((rideData, index) => (
+                        <SavedRideCard
+                            key={index}
+                            data={rideData}
+                            rides={rides}
+                            setRides={setRides}
+                        />
+                    ))}
+                </div>
+            )}
         </div>
-    )
-
+    );
 }
 
 const mapStateToProps = (state) => ({
     savedRides: state.user.user.savedRides
 });
 
-export default connect(mapStateToProps)(SavedRides)
-
-
-
-
+export default connect(mapStateToProps)(SavedRides);
--- src/Components/Rides/SavedRide/SavedRide.spec.js
+++ src/Components/Rides/SavedRide/SavedRide.spec.js
@@ -3,7 +3,7 @@ import SavedRide from "./SavedRide";
 import { Provider } from "react-redux";
 import { createStore, combineReducers } from "redux";
 import { BrowserRouter as Router } from "react-router-dom";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import { UserReducer as reducer } from "./../../../Redux/Reducers/UserReducer";
 
 function renderWithRedux(
@@ -22,6 +22,17 @@ function renderWithRedux(
     };
 }
 
-test("should Render Save Ride Component", () => {
+test("should Render Save Ride Component", async () => {
     const Wrapper = renderWithRedux(<SavedRide />);
+    const newRideBtn = Wrapper.getByText("Add New ride");
+
+    expect(newRideBtn).toBeDefined();
+    expect(Wrapper.getByText("My saved rides...").nodeName).toEqual("H1");
+
+    //Click button to render a AddLocation component
+    await fireEvent.click(newRideBtn);
+
+
+    expect(Wrapper.getByPlaceholderText("location name").nodeName).toBe("INPUT");
+    expect(Wrapper.getByText("Save and Close").nodeName).toBeDefined();
 });
