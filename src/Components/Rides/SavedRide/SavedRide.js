d194b312cd02a4f90ca2d0620de97dc0c371401b
Ruth Poliakon <r.poliakon@gmail.com>
2020-05-07 17:44:04 -0600

    remove export defaul duplicate

--- src/Components/Rides/SavedRide/SavedRide.js
+++ src/Components/Rides/SavedRide/SavedRide.js
@@ -1,58 +1,46 @@
-
-import React, { useState, useEffect } from 'react'
-import SavedRideCard from '../SavedRideCard/SavedRideCard'
-import { connect } from 'react-redux'
-import AddLocationName from '../SavedRideCard/AddFavoriteLocation'
-import { getFavorites } from '../../../Redux/Actions/LocationActions'
+import React, { useState, useEffect } from "react";
+import SavedRideCard from "../SavedRideCard/SavedRideCard";
+import { connect } from "react-redux";
+import AddLocationName from "../SavedRideCard/AddFavoriteLocation";
+import { getFavorites } from "../../../Redux/Actions/LocationActions";
 import "./SavedRide.scss";
 
-
-
-
-
 function SavedRides(props) {
-    const [show, setShow] = useState(false)
+    const [show, setShow] = useState(false);
     // const [favoriteLocations, setFavoriteLocations] = useState()
     useEffect(() => {
-        props.getFavorites()
-
-    }, [])
-
-
+        props.getFavorites();
+    }, []);
 
     const toggleShow = () => {
-        setShow(!show)
-        console.log(show)
-    }
+        setShow(!show);
+        console.log(show);
+    };
 
-    console.log(props)
-    
+    console.log(props);
+
     return (
         <div>
+            {show ? (
+                <AddLocationName toggle={toggleShow} />
+            ) : (
+                <div className="saved-rides">
+                    <section className="my-saved">
+                        <h1>My saved rides...</h1>
+                        <button onClick={toggleShow}>Add New ride</button>
+                    </section>
 
-            {show ? (<AddLocationName toggle={toggleShow} />) : (< div className='saved-rides' >
-                <section className='my-saved'>
-                    <h1>My saved rides...</h1>
-                    <button onClick={toggleShow}>Add New ride</button>
-                </section>
-
-                {props.favoriteLocation
-                    .map((rideData, index) => <SavedRideCard key={index} data={rideData} />)}
-
-            </div >)}
+                    {props.favoriteLocation.map((rideData, index) => (
+                        <SavedRideCard key={index} data={rideData} />
+                    ))}
+                </div>
+            )}
         </div>
     );
 }
 
 const mapStateToProps = (state) => ({
-
     favoriteLocation: state.locations.favoriteLocation
-
 });
 
-export default connect(mapStateToProps, { getFavorites })(SavedRides)
-
-
-
-
-export default connect(mapStateToProps)(SavedRides);
+export default connect(mapStateToProps, { getFavorites })(SavedRides);
