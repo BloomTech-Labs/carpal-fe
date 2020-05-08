e96e91d95252f341131bea6edacb4e78de10f976
Carl Redding <credding07@gmail.com>
2020-05-08 01:05:38 -0500

    touched forms.scss and applied better padding for the formik-container element. Login.js and signup.js had the images uploaded to the components instead of scss having them as a background image. adjusted fitting has been applied and stacked over p and link tags. looks cleaner. opacity is up on cuties as well. Also adjusted the hover to show the lightgreen color for the riderequestscard.scss. Looking into duplicate styling loop of code and why its re-rendering it so much.

--- src/Components/Login-SignUp/Login.js
+++ src/Components/Login-SignUp/Login.js
@@ -1,10 +1,11 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
-import { withFormik, Form, Field } from "formik";
+import { withFormik, Form } from "formik";
 import * as Yup from "yup";
 import "./Login.scss";
 import LabelField from "../Form-Components/LabelField";
+import cuties from "../../img/background/Cutie-Trio-Bckgrnd.png";
 
 import { LogInAction } from "../../Redux/Actions/UserAction";
 
@@ -53,6 +54,8 @@ function Login(props) {
             </Form>
 
             <div className="module-nav">
+                <img className="module-cuties" src={cuties} alt="cuties" />
+
                 <p className="module-p">
                     New to the website?
                     <Link className="signup-link" to="/signup">
--- src/Components/Login-SignUp/SignUp.js
+++ src/Components/Login-SignUp/SignUp.js
@@ -5,6 +5,7 @@ import { withFormik, Form } from "formik";
 import * as Yup from "yup";
 import "./SignUp.scss";
 import LabelField from "../Form-Components/LabelField";
+import cuties from "../../img/background/Cutie-Trio-Bckgrnd.png";
 
 import { SignUpAction } from "../../Redux/Actions/UserAction";
 
@@ -65,6 +66,7 @@ function SignUp(props) {
                 </a>
             </Form>
             <div className="module-nav">
+                <img className="module-cuties" src={cuties} alt="cuties" />
                 <p className="module-p">
                     Already a user?
                     <Link className="login-link" to="/login">
