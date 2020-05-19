e4a891b8059dd1393d4508935fa42aa570fc601a
Ruth Poliakon <r.poliakon@gmail.com>
2020-05-18 18:17:56 -0600

    save ride action created and implemented in RideFind
    Co-authored-by: Dennis Padiernos <depadiernos@gmail.com>

--- src/Redux/Actions/LocationActions.js
+++ src/Redux/Actions/LocationActions.js
@@ -4,9 +4,11 @@ export const ADD_LOCATION = "ADD_LOCATION";
 export const DELETE_LOCATION = "DELETE_LOCATION";
 export const EDIT_LOCATION = "EDIT_LOCATION";
 export const REQUEST_START = "REQUEST_START";
+export const REQUEST_SUCCESS = "REQUEST_SUCCESS";
 export const REQUEST_ERROR = "REQUEST_ERROR";
 export const SAVE_ROUTE = "SAVE_ROUTE";
 export const SET_STOPS = "SET_STOPS";
+export const SAVE_RIDE = "SAVE_RIDE";
 
 export function getFavorites() {
     return (dispatch) => {
@@ -79,6 +81,7 @@ export function EditLocation(location) {
     };
 }
 
+// poly line routing
 export function currentRoute(start, end) {
     return (dispatch) => {
         dispatch({ type: SAVE_ROUTE, payload: { start, end } });
@@ -87,6 +90,34 @@ export function currentRoute(start, end) {
 
 export function setStops(stopsArray) {
     return (dispatch) => {
-        dispatch({type: SET_STOPS, payload: stopsArray})
-    }
+        dispatch({ type: SET_STOPS, payload: stopsArray });
+    };
+}
+
+// save a ride given start and end lat and long
+export function saveRide({ start_location_id, end_location_id }, props) {
+    return async (dispatch) => {
+        try {
+            dispatch({ type: REQUEST_START });
+            // post start and end lat and long to /locations to obtain ids for each
+            const start = await api().post("/locations", {
+                lat: start_location_id[0],
+                long: start_location_id[1]
+            });
+            const end = await api().post("/locations", {
+                lat: end_location_id[0],
+                long: end_location_id[1]
+            });
+            // send the two recieved start and end ids to the users rides to create a new saved ride
+            const savedRide = await api().post("/users/rides", {
+                start_location_id: start.data.id,
+                end_location_id: end.data.id
+            });
+            dispatch({ type: SAVE_RIDE, payload: savedRide.data });
+            dispatch({ type: REQUEST_SUCCESS });
+            props.history.push("/saved");
+        } catch (err) {
+            dispatch({ type: REQUEST_ERROR, payload: err });
+        }
+    };
 }
