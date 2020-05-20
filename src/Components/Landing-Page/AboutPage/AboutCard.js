fc8f4e7caacd2d8f8c2e37fedb9923c9fdcf992b
Ruth Poliakon <r.poliakon@gmail.com>
2020-05-19 22:01:55 -0600

    AboutCard testing

--- src/Components/Landing-Page/AboutPage/AboutCard.js
+++ src/Components/Landing-Page/AboutPage/AboutCard.js
@@ -3,9 +3,9 @@ import ImagesSocials from "./ImagesSocials";
 
 const AboutCard = (props) => {
     return (
-        <div className="awesome-aliens-box">
-            <div className="left">
-                <h2>{props.carpaller.name}</h2>
+        <div className="awesome-aliens-box" data-testid="awesome-aliens-box">
+            <div className="left" data-testid="left">
+                <h2 data-testid="carpaller-name">{props.carpaller.name}</h2>
                 <h3 className="team-title">{props.carpaller.title}</h3>
                 <div className="hide-tablet show-desktop">
                     <ImagesSocials carpaller={props.carpaller} />
@@ -13,7 +13,7 @@ const AboutCard = (props) => {
 
                 <p>{props.carpaller.bio}</p>
             </div>
-            <div className="right  show-tablet hide-desktop">
+            <div className="right show-tablet hide-desktop" data-testid="right">
                 <ImagesSocials carpaller={props.carpaller} />
             </div>
         </div>
